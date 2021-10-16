3b92681cf1dbaebcd6403bed362ecb3e339b3cb7
Marisela Simon <caminosantiagolevante@gmail.com>
2021-10-16 18:48:41 +0200

    primero

---
+++ libro/book/js/book.js
@@ -0,0 +1,127 @@
+function updateDepth(book, newPage) {
+
+	var page = book.turn('page'),
+		pages = book.turn('pages'),
+		depthWidth = 16*Math.min(1, page*2/pages);
+
+		newPage = newPage || page;
+
+	if (newPage>3)
+		$('.sj-book .p2 .depth').css({
+			width: depthWidth,
+			left: 20 - depthWidth
+		});
+	else
+		$('.sj-book .p2 .depth').css({width: 0});
+
+		depthWidth = 16*Math.min(1, (pages-page)*2/pages);
+
+	if (newPage<pages-3)
+		$('.sj-book .p67 .depth').css({
+			width: depthWidth,
+			right: 20 - depthWidth
+		});
+	else
+		$('.sj-book .p67 .depth').css({width: 0});
+
+}
+
+function loadPage(page) {
+
+	$.ajax({url: 'pages/page' + page + '.html'}).
+		done(function(pageHtml) {
+			$('.sj-book .p' + page).html(pageHtml.replace('libro/book/', ''));
+		});
+
+}
+
+function addPage(page, book) {
+
+	var id, pages = book.turn('pages');
+
+	if (!book.turn('hasPage', page)) {
+
+		var element = $('<div />',
+			{'class': 'own-size',
+				css: {width:560, height: 462}
+			}).
+			html('<div class="loader"></div>');
+
+		if (book.turn('addPage', element, page)) {
+			loadPage(page);
+		}
+
+	}
+}
+
+function numberOfViews(book) {
+
+	return book.turn('pages') / 2 + 1;
+
+}
+
+function getViewNumber(book, page) {
+
+	return parseInt((page || book.turn('page'))/2 + 1, 10);
+
+}
+
+function zoomHandle(e) {
+
+	if ($('.sj-book').data().zoomIn)
+		zoomOut();
+	else if (e.target && $(e.target).hasClass('zoom-this')) {
+		zoomThis($(e.target));
+	}
+
+}
+
+function moveBar(yes) {
+	if (Modernizr && Modernizr.csstransforms) {
+		$('#slider .ui-slider-handle').css({zIndex: yes ? -1 : 10000});
+	}
+}
+
+function setPreview(view) {
+
+	var previewWidth = 115,
+		previewHeight = 43,
+		previewSrc = 'pages/preview.jpg',
+		preview = $(_thumbPreview.children(':first')),
+		numPages = (view==1 || view==$('#slider').slider('option', 'max')) ? 1 : 2,
+		width = (numPages==1) ? previewWidth/2 : previewWidth;
+
+	_thumbPreview.
+		addClass('no-transition').
+		css({width: width + 15,
+			height: previewHeight + 15,
+			top: -previewHeight - 30,
+			left: ($($('#slider').children(':first')).width() - width - 15)/2
+		});
+
+	preview.css({
+		width: width,
+		height: previewHeight
+	});
+
+	if (preview.css('background-image')==='' ||
+		preview.css('background-image')=='none') {
+
+		preview.css({backgroundImage: 'url(' + previewSrc + ')'});
+
+		setTimeout(function(){
+			_thumbPreview.removeClass('no-transition');
+		}, 0);
+
+	}
+
+	preview.css({backgroundPosition:
+		'0px -'+((view-1)*previewHeight)+'px'
+	});
+}
+
+function isChrome() {
+
+	return navigator.userAgent.indexOf('Chrome')!=-1;
+
+}
